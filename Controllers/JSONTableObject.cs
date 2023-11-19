4f2fe6931122d7d5a6dea40c5b71dbf52420925a
SandroGitA <maktavish74@mail.ru>
2023-11-19 15:08:48 +0500

    Создана модель для привязки сложных типов

---
+++ Controllers/JSONTableObject.cs
@@ -0,0 +1,18 @@
+﻿namespace AnalogGoogleTableCSharp.Controllers
+{
+    public class JSONTableObject
+    {
+        public id_cell ID_cell { get; set; }
+        public data Data { get; set; }
+
+        public class id_cell
+        {
+            public int id { get; set; }
+        }
+
+        public class data
+        {
+            public string text { get; set; }
+        }
+    }
+}
