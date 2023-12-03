31e9710196be2dbd223eafa550b5748bd0808e04
SandroGitA <maktavish74@mail.ru>
2023-12-03 20:29:33 +0500

    add MySQLConnect class

---
+++ Controllers/SQLConnect.cs
@@ -0,0 +1,45 @@
+﻿using MySql.Data.MySqlClient;
+
+namespace AnalogGoogleTableCSharp.Controllers
+
+{
+    /// <summary>
+    /// Подключение к базе данных
+    /// </summary>
+    public class SQLConnect
+    {
+        public string username;
+        public string password;
+
+        public string server = "localhost";
+        public string database_name = "DataTable";
+        public int port;
+
+        public string conncetionString = "";
+        public string connectrionStatus = "";
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        SQLConnect() { }
+
+        /// <summary>
+        /// Метод для подключения к БД
+        /// </summary>
+        /// <returns></returns>
+        public MySqlConnection ReturnSQLConnection()
+        {
+            MySqlConnection mySqlConnection = new MySqlConnection(conncetionString);
+            try
+            {
+                mySqlConnection.Open();
+            }
+            catch (Exception e)
+            {
+                connectrionStatus = e.Message;
+            }
+
+            return mySqlConnection;
+        }
+    }
+}
