86e49151538188b57fd5ef47de603fbf57d15d3a
SandroGitA <maktavish74@mail.ru>
2023-12-06 21:54:30 +0500

    Addition method Get

--- Controllers/v1Controller.cs
+++ Controllers/v1Controller.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -8,14 +9,41 @@ namespace AnalogGoogleTableCSharp.Controllers
     [ApiController]
     public class v1Controller : ControllerBase
     {
+        //При запуске приложения создаем подключение к БД
+        SQLConnect connect = new SQLConnect();
+
         // GET: api/<ValuesController>
         [HttpGet]
         public IEnumerable<string> Get()
-        {
-            SQLConnect connect = new SQLConnect();            
+        {                      
             var str = connect.ReturnSQLConnection();
 
-            return new string[] { "value1", "value2" };
+            //Создаем объект команды
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "select * from DataTable";
+            cmd.Connection = str;
+
+            SqlDataReader dr = cmd.ExecuteReader();
+
+            object js = "";
+
+            while (dr.Read())
+            {
+                object id = dr.GetValue(0);
+                object data = dr.GetValue(1);
+                object coordinate = dr.GetValue(2);
+
+                js = new JSONTableObject(){
+                    Data = (JSONTableObject.data)data, ID_cell = (JSONTableObject.id_cell)id
+                };
+            }
+            
+            dr.Close();
+            str.Close();
+
+            //return new string[] { "value1", "value2" };
+
+            return (IEnumerable<string>)js;
         }
 
         // GET api/<ValuesController>/5
