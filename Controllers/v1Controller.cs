d8987fac52483d9574d827db5ffd4e34e8549163
SandroGitA <maktavish74@mail.ru>
2023-12-10 14:54:36 +0500

    Add methid Get

--- Controllers/v1Controller.cs
+++ Controllers/v1Controller.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -10,40 +11,39 @@ namespace AnalogGoogleTableCSharp.Controllers
     public class v1Controller : ControllerBase
     {
         //При запуске приложения создаем подключение к БД
-        SQLConnect connect = new SQLConnect();
+        SQLConnect connect = new SQLConnect();        
 
         // GET: api/<ValuesController>
         [HttpGet]
-        public IEnumerable<string> Get()
+        public string Get()
         {                      
-            var str = connect.ReturnSQLConnection();
+            var connection = connect.ReturnSQLConnection();
 
             //Создаем объект команды
             SqlCommand cmd = new SqlCommand();
             cmd.CommandText = "select * from DataTable";
-            cmd.Connection = str;
+            cmd.Connection = connection;
 
-            SqlDataReader dr = cmd.ExecuteReader();
+            SqlDataReader reader = cmd.ExecuteReader();
 
-            object js = "";
+            //Создаем лист объектов
+            List<JSONTableObject> listCoordinates = new List<JSONTableObject>();
 
-            while (dr.Read())
+            while (reader.Read())
             {
-                object id = dr.GetValue(0);
-                object data = dr.GetValue(1);
-                object coordinate = dr.GetValue(2);
+                object id = reader.GetValue(0);
+                object data = reader.GetValue(1);
+                object coordinate = reader.GetValue(2);
 
-                js = new JSONTableObject(){
-                    Data = (JSONTableObject.data)data, ID_cell = (JSONTableObject.id_cell)id
-                };
+                listCoordinates.Add(new JSONTableObject { cd = (JSONTableObject.CellData)data, cc = (JSONTableObject.CellCoordinate)coordinate });
             }
             
-            dr.Close();
-            str.Close();
+            reader.Close();
+            connection.Close();
 
-            //return new string[] { "value1", "value2" };
+            return JsonConvert.SerializeObject(listCoordinates);
 
-            return (IEnumerable<string>)js;
+            //return new string[] { "value1", "value2" };            
         }
 
         // GET api/<ValuesController>/5
